3838a0db817bafe64c228475b387108815083a9e
bilaalukis <bilaalukis123@gmail.com>
2021-04-05 15:22:37 +0900

    Added some htm elements for frontpage and connected css file in main.go. Also, installed fresh

--- main.go
+++ main.go
@@ -1,25 +1,37 @@
 package main
 
 import (
-	"net/http"  // Provides HTTP client and server implementation for use in the app
-	"os" // Allows operating system functionalities
+	"html/template"
+	"log"
+	"net/http" // Provides HTTP client and server implementation for use in the app
+	"os"       // Allows operating system functionalities
+
+	"github.com/joho/godotenv"
 )
+
+ var tpl = template.Must(template.ParseFiles("index.html"))  // Package level variable. Parses the index.html file as a template
 
 // w represents the "res" and r is "req"
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, nil)
 }
 
 func main() {
+	err := godotenv.Load()  // Reads the .env file
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "5000"
 	}
 
+	fs := http.FileServer(http.Dir("assets"))  // Pass the directory where all static files are saved to the file server
+
 	mux := http.NewServeMux()  // http request multiplexer (need to read up on it more)
-	
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))  // StripPrefix strips off the specified prefix
 	mux.HandleFunc("/", indexHandler)
-	
 	http.ListenAndServe(":"+port, mux)  // starts the server on port
 }
 
