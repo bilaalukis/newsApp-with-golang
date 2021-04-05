99ccf8b9ea7f7e065fcfc6540b3184be92744054
bilaalukis <bilaalukis123@gmail.com>
2021-04-05 15:46:13 +0900

    Added search route

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http" // Provides HTTP client and server implementation for use in the app
-	"os"       // Allows operating system functionalities
+	"net/url"
+	"os" // Allows operating system functionalities
 
 	"github.com/joho/godotenv"
 )
@@ -16,13 +18,33 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
+// function to deal with users' searches
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")  //Users' query
+	page := params.Get("page")  // Page through results
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchQuery)
+	fmt.Println("Page is: ", page)
+}
+
+// This is like the renderer for the main page
 func main() {
 	err := godotenv.Load()  // Reads the .env file
 	if err != nil {
 		log.Println("Error loading .env file")
 	}
 
-	port := os.Getenv("PORT")
+	port := os.Getenv("PORT")  // Sets the port
 	if port == "" {
 		port = "5000"
 	}
@@ -31,6 +53,7 @@ func main() {
 
 	mux := http.NewServeMux()  // http request multiplexer (need to read up on it more)
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))  // StripPrefix strips off the specified prefix
+	mux.HandleFunc("/search", searchHandler)
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)  // starts the server on port
 }
